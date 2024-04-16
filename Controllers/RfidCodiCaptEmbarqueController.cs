cd4214d6b14cf9b1e71b830e3f6b014e937b980f
Oziel Pacheco <ozielpacheco.m@gmail.com>
2024-04-15 18:34:15 -0600

    Se agrega el tercer reporte agrupado por viaje y su exportacion a excel

--- Controllers/RfidCodiCaptEmbarqueController.cs
+++ Controllers/RfidCodiCaptEmbarqueController.cs
@@ -19,7 +19,7 @@ namespace test_lester02.Controllers
         }
 
         [HttpPost]
-        public IActionResult GenerarReporte(string startDate, string endDate, bool agrupar)
+        public IActionResult GenerarReporte(string startDate, string endDate, string agrupar)
         {
             
             DateTime startDateTime = DateTime.Parse(startDate);
@@ -28,26 +28,38 @@ namespace test_lester02.Controllers
                     : DateTime.Parse(endDate).AddDays(1).AddMilliseconds(-1);
             List<string> headersList = new List<string>();
 
-            if (agrupar)
+            switch (agrupar)
             {
-                var report = ConsultaAgrupada(startDateTime, endDateTime);
+                case "agruparAcronimo":
+                        var report = ConsultaAgrupada(startDateTime, endDateTime);
 
-                headersList.Add("Acronimo");
-                headersList.Add("Viaje");
-                headersList.Add("Cantidad");
+                        headersList.Add("Acronimo");
+                        headersList.Add("Viaje");
+                        headersList.Add("Cantidad");
 
-                ViewBag.Headers = headersList;
-                ViewBag.Report = report;
-            }
-            else
-            {
-                var report = test(startDateTime, endDateTime);
-                headersList.Add("RFID");
-                headersList.Add("Acrónimo");
-                headersList.Add("Hora de lectura");
+                        ViewBag.Headers = headersList;
+                        ViewBag.Report = report;
+                    break;
+                case "agruparViaje":
+                        report = ConsultaAgrupadaNoViaje(startDateTime, endDateTime);
 
-                ViewBag.Headers = headersList;
-                ViewBag.Report = report;
+                        headersList.Add("Viaje");
+                        headersList.Add("Cantidad");
+                        headersList.Add("Fecha");
+
+                        ViewBag.Headers = headersList;
+                        ViewBag.Report = report;
+                    break;
+                case "sinAgrupar":
+                        report = test(startDateTime, endDateTime);
+
+                        headersList.Add("RFID");
+                        headersList.Add("Acrónimo");
+                        headersList.Add("Hora de lectura");
+
+                        ViewBag.Headers = headersList;
+                        ViewBag.Report = report;
+                    break;
             }
 
             ViewBag.StartDateTime = startDateTime;
@@ -77,6 +89,21 @@ namespace test_lester02.Controllers
                         {
                             Acronimo = g.Key.Acronimo,
                             Viaje = g.Key.Viaje,
+                            Cantidad = g.Count()
+                        })
+                        .ToList<Object>();
+            return report;
+        }
+
+        public List<Object> ConsultaAgrupadaNoViaje(DateTime startDate, DateTime endDate)
+        {
+            var report = _context.TblRfidCodiCaptEmbarques
+                        .Where(r => r.FechaLectura >= startDate && r.FechaLectura <= endDate)
+                        .GroupBy(r => r.Viaje )
+                        .Select(g => new
+                        {
+                            Viaje = g.Key,
+                            Fecha = g.Min(r => r.FechaLectura),
                             Cantidad = g.Count()
                         })
                         .ToList<Object>();
@@ -143,7 +170,8 @@ namespace test_lester02.Controllers
 
             //Agregar encabezados
             worksheet.Cells[1, 1].Value = "Acronimo";
-            worksheet.Cells[1, 2].Value = "Cantidad";
+            worksheet.Cells[1, 2].Value = "Acronimo";
+            worksheet.Cells[1, 3].Value = "Cantidad";
 
             //Agregar datos
             int row = 2;
@@ -151,7 +179,8 @@ namespace test_lester02.Controllers
             {
                 var properties = item.GetType().GetProperties();
                 worksheet.Cells[row, 1].Value = properties.First(p => p.Name == "Acronimo").GetValue(item);
-                worksheet.Cells[row, 2].Value = properties.First(p => p.Name == "Cantidad").GetValue(item);
+                worksheet.Cells[row, 2].Value = properties.First(p => p.Name == "Viaje").GetValue(item);
+                worksheet.Cells[row, 3].Value = properties.First(p => p.Name == "Cantidad").GetValue(item);
                 row++;
             }
 
@@ -162,6 +191,49 @@ namespace test_lester02.Controllers
 
             //devolver el archivo excel a un archivo descargable
             string fileName = "Reporte_agrupado.xlsx";
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            return File(stream, contentType, fileName);
+        }
+
+        [HttpPost]
+        public IActionResult GenerarReporteAgrupadoNoViajeExcel(string startDate, string endDate)
+        {
+            DateTime startDateTime = DateTime.Parse(startDate);
+            DateTime endDateTime = string.IsNullOrEmpty(endDate)
+                    ? DateTime.Today.AddDays(1).AddMilliseconds(-1)
+                    : DateTime.Parse(endDate).AddDays(1).AddMilliseconds(-1);
+
+            var report = ConsultaAgrupadaNoViaje(startDateTime, endDateTime);
+
+            //Crear archivo de excel
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            ExcelPackage excelPackage = new ExcelPackage();
+            ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("report");
+
+            //Agregar encabezados
+            worksheet.Cells[1, 1].Value = "Viaje";
+            worksheet.Cells[1, 2].Value = "Cantidad";
+            worksheet.Cells[1, 3].Value = "Fecha";
+
+            //Agregar datos
+            int row = 2;
+            foreach (var item in report)
+            {
+                var properties = item.GetType().GetProperties();
+                worksheet.Cells[row, 1].Value = properties.First(p => p.Name == "Viaje").GetValue(item);
+                worksheet.Cells[row, 2].Value = properties.First(p => p.Name == "Cantidad").GetValue(item);
+                worksheet.Cells[row, 3].Value = properties.First(p => p.Name == "Fecha").GetValue(item);
+                row++;
+            }
+
+            //Guardar archivo
+            MemoryStream stream = new MemoryStream();
+            excelPackage.SaveAs(stream);
+            stream.Position = 0;
+
+            //devolver el archivo excel a un archivo descargable
+            string fileName = "Reporte_agrupado_noviaje.xlsx";
             string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 
             return File(stream, contentType, fileName);
